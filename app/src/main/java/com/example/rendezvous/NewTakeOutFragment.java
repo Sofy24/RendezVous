e43f34a5eb12ffcbf6a426a71a447e3a5265cf85
Sofy24 <sofia.tosi4@studio.unibo.it>
2022-04-23 19:58:54 +0200

    did some stuff

--- app/src/main/java/com/example/rendezvous/NewTakeOutFragment.java
+++ app/src/main/java/com/example/rendezvous/NewTakeOutFragment.java
@@ -1,6 +1,86 @@
 package com.example.rendezvous;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class NewTakeOutFragment extends Fragment {
+    private static final String LOG_TAG = "NewTakeOutFragment";
+
+
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment,
+     * @param container          If non-null, this is the parent view that the fragment's UI should be attached to.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_take_out, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FragmentActivity activity = getActivity();
+    }
+        /*if (activity != null) {
+            Utilities.setUpToolbar((AppCompatActivity) activity, getString(R.string.app_name));
+
+            setRecyclerView(activity);
+
+            listViewModel = new ViewModelProvider(activity).get(ListViewModel.class);
+            listViewModel.getCardItems().observe(activity, new Observer<List<CardItem>>() {
+                @Override
+                public void onChanged(List<CardItem> cardItems) {
+                    adapter.setData(cardItems);
+                }
+            });*/
+
+            /*FloatingActionButton floatingActionButton = view.findViewById(R.id.fab_add);
+            floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Utilities.insertFragment((AppCompatActivity) activity, new AddFragment(),
+                            AddFragment.class.getSimpleName());
+                }
+            });
+        } else {
+            Log.e(LOG_TAG, "Activity is null");
+        }*/
+
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    
 }

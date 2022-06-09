9c9a47558166dd65f8180a5a19662613dd8f3d0f
Sofy24 <sofia.tosi4@studio.unibo.it>
2022-06-10 00:04:02 +0200

    A fantastic navigation drawer

--- app/src/main/java/com/example/rendezvous/CalendarFragment.java
+++ app/src/main/java/com/example/rendezvous/CalendarFragment.java
@@ -78,7 +78,6 @@ public class CalendarFragment extends Fragment {
                     LocalDate currentDay = LocalDate.of(year, month, day);
                     Toast.makeText(getActivity(),"y = " + year + ", m = " + month + ", d = " + day,Toast.LENGTH_SHORT).show();
                     Toast.makeText(getActivity(), "" + currentDay,Toast.LENGTH_SHORT).show();
-
             }
         });
         return view;
@@ -254,7 +253,6 @@ public class CalendarFragment extends Fragment {
         dLayout = (DrawerLayout) activity.findViewById(R.id.drawer_layout); // initiate a DrawerLayout
         NavigationView navView = (NavigationView) activity.findViewById(R.id.navigation); // initiate a Navigation View
 // implement setNavigationItemSelectedListener event on NavigationView
-
 //        Workaround per impedire che il fragment diventi timido quando la navigationView esce fuori
         dLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
             @Override

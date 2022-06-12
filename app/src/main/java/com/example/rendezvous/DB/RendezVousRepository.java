1bca4da5fa2735b61ada07c22fdac1cc1762ac5b
Sofy24 <sofia.tosi4@studio.unibo.it>
2022-06-12 13:06:28 +0200

    Fixed the color of the empty drawer animation

--- app/src/main/java/com/example/rendezvous/DB/RendezVousRepository.java
+++ app/src/main/java/com/example/rendezvous/DB/RendezVousRepository.java
@@ -3,30 +3,13 @@ package com.example.rendezvous.DB;
 import android.app.Application;
 
 public class RendezVousRepository {
-    private RendezVousDB rendezVousDB;
+    private final RendezVousDB rendezVousDB;
 
-    private DatabaseDAO databaseDAO;
+    private final DatabaseDAO databaseDAO;
 
     public RendezVousRepository(Application application) {
         rendezVousDB = RendezVousDB.getInstance(application);
         databaseDAO =  rendezVousDB.databaseDAO();
     }
 
-//    public void addUser(final String UID, final String username, final String psw){
-//        this.rendezVousDB.executor.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                databaseDAO.insertUser(UID, username, psw);
-//            }
-//        });
-//    }
-
-//    public void addCircle(final String C_ID, final String C_name, final String C_color){
-//        this.rendezVousDB.executor.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                databaseDAO.insertCircle(C_ID, C_name, C_color);
-//            }
-//        });
-//    }
 }
